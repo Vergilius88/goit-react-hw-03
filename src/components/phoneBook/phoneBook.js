92bec24d1b22a34978aae0177cecb0c9548bdd48
Nikolaienko Ihor <nikolasha8876@gmail.com>
2021-03-09 17:54:41 +0200

    implemented writing / reading to local storage

--- src/components/phoneBook/phoneBook.js
+++ src/components/phoneBook/phoneBook.js
@@ -1,5 +1,4 @@
 import { Component } from "react";
-
 import createContact from "./createContact/createContact";
 import ContactUsers from "./contactUsers/contactUsers";
 import FormOfAdding from "./formOfAdding/formOfAdding";
@@ -14,6 +13,20 @@ export default class PhoneBook extends Component {
     contacts: [],
     filter: "",
   };
+
+  componentDidMount() {
+    const contactsLocalStorage = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contactsLocalStorage);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = (newContact) => {
     this.setState((prevState) => {
